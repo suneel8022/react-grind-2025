38110fd12a73ab3bcb3ed52d2965efbf03a2834b
Suneel <suneelhere8022@gmail.com>
2025-01-12 00:19:32 +0530

    added a sample count button

--- 06-click-events/src/Button.jsx
+++ 06-click-events/src/Button.jsx
@@ -1,14 +1,14 @@
 function Button() {
 
-    const handleClick = () => console.log("Ouch!!");
-    
-    const handleClick2 = (name) => console.log(`${name} stop clicking me`);
-    
+    let count=0;
 
-    return(
-        // <button onClick={handleClick}>Click me</button>
-        <button onClick={() => handleClick2("Bro")}>Click me</button>
+        const handleClick = () => console.log(`Count is: ${count++}`);
+        
+
+    return (
+        <button onClick={handleClick}>Click me</button>
     );
+
 }
 
 export default Button
