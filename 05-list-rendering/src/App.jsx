007c449effd2dd16a9bfaff1436636b56efbe804
Suneel <suneelhere8022@gmail.com>
2025-01-11 22:56:37 +0530

    added props for making reusable

--- 05-list-rendering/src/App.jsx
+++ 05-list-rendering/src/App.jsx
@@ -1,8 +1,19 @@
 import List from "./List.jsx"
 
 function App() {  
+
+      const fruits = [
+        {id:1,name:"orange", calories:95},
+        {id:2,name:"apple", calories:55},
+        {id:3,name:"banana", calories:75},
+        {id:4,name:"grapes", calories:21},
+        {id:5,name:"papaya", calories:66}
+    ];
+
+
+
   return(
-      <List/>
+      <List items={fruits} category="Fruits"/>
   )
 }
 
--- 05-list-rendering/src/List.jsx
+++ 05-list-rendering/src/List.jsx
@@ -1,35 +1,18 @@
-function List(){
+function List(props){
 
-    const fruits = [
-        {id:1,name:"orange", calories:95},
-        {id:2,name:"apple", calories:55},
-        {id:3,name:"banana", calories:75},
-        {id:4,name:"grapes", calories:21},
-        {id:5,name:"papaya", calories:66}
-    ];
+    const category = props.category;
+    const itemList = props.items;
 
-            // string sort
-    // fruits.sort((a,b) => a.name.localeCompare(b.name)); // asc by name
-    // fruits.sort((a,b) => b.name.localeCompare(a.name)); // desc by name
-            // number sort
-    fruits.sort((a,b) => a.calories-b.calories)  // asc by calories
-    // fruits.sort((a,b) => b.calories-a.calories)  // desc by calories
+    const listItems = itemList.map(item => <li key={item.id}>
+                                                            {item.name}
+                                                            :&nbsp; <b>{item.calories}</b></li>);
 
-                    // filter
-
-    const lowCalFruits = fruits.filter(fruit => fruit.calories < 70)
-
-                        // for normal records display using map
-    // const listItems = fruits.map(fruit => <li key={fruit.id}>
-    //                                                         {fruit.name}
-    //                                                         :&nbsp; <b>{fruit.calories}</b></li>);
-
-                    // for filtered records display using map
-    const listItems = lowCalFruits.map(lowCalFruit => <li key={lowCalFruit.id}>
-                                                            {lowCalFruit.name}
-                                                            :&nbsp; <b>{lowCalFruit.calories}</b></li>);
-
-    return(<ol>{listItems}</ol>);   
+    return(
+        <>
+        <h3>{category}</h3>
+    <ol>{listItems}</ol>
+        </>
+);   
 }
 
 export default List
