3e65092161d71babc2b1f8eaa3d651c5d2a024df
Suneel <suneelhere8022@gmail.com>
2025-01-11 22:02:57 +0530

    used basic filter, map operations

--- 05-list-rendering/src/List.jsx
+++ 05-list-rendering/src/List.jsx
@@ -1,10 +1,33 @@
 function List(){
 
-    const fruits = ["orange","apple","banana","grapes","papaya"]
+    const fruits = [
+        {id:1,name:"orange", calories:95},
+        {id:2,name:"apple", calories:55},
+        {id:3,name:"banana", calories:75},
+        {id:4,name:"grapes", calories:21},
+        {id:5,name:"papaya", calories:66}
+    ];
 
+            // string sort
+    // fruits.sort((a,b) => a.name.localeCompare(b.name)); // asc by name
+    // fruits.sort((a,b) => b.name.localeCompare(a.name)); // desc by name
+            // number sort
+    fruits.sort((a,b) => a.calories-b.calories)  // asc by calories
+    // fruits.sort((a,b) => b.calories-a.calories)  // desc by calories
 
-    fruits.sort();
-    const listItems = fruits.map(fruit => <li>{fruit}</li>);
+                    // filter
+
+    const lowCalFruits = fruits.filter(fruit => fruit.calories < 70)
+
+                        // for normal records display using map
+    // const listItems = fruits.map(fruit => <li key={fruit.id}>
+    //                                                         {fruit.name}
+    //                                                         :&nbsp; <b>{fruit.calories}</b></li>);
+
+                    // for filtered records display using map
+    const listItems = lowCalFruits.map(lowCalFruit => <li key={lowCalFruit.id}>
+                                                            {lowCalFruit.name}
+                                                            :&nbsp; <b>{lowCalFruit.calories}</b></li>);
 
     return(<ol>{listItems}</ol>);   
 }
