070cfbca3f9d9f6ad3b4e2826ca42f5e4a7eaf97
Suneel <suneelhere8022@gmail.com>
2025-01-12 22:04:57 +0530

    added boolean toggle

--- 07-use-state/src/MyComponent.jsx
+++ 07-use-state/src/MyComponent.jsx
@@ -2,15 +2,21 @@ import React,{useState} from "react";
 
 function MyComponent(){
 
-    const[name, setName] = useState("Guest")
+    const [name, setName] = useState("Guest")
     const [age,setAge] = useState(0)
+    const [isEmployed,setIsEmployed] = useState(false)
 
     const updateName = () =>{
         setName("Suneel")
     }
 
     const updateAge = () =>{
-        setAge(25)
+        // setAge(25)
+        setAge(age + 1)
+    }
+
+    const updateStatus = ()=>{
+        setIsEmployed(!isEmployed)
     }
 
     return(
@@ -20,7 +26,11 @@ function MyComponent(){
 
         <p>Age: {age}</p>
         <button onClick={updateAge}>Set Age</button>
+
+        <p>Is Employes: {isEmployed ? "Yes" : "No"}</p>
+        <button onClick={updateStatus}>Toggle Status</button>
         </>
+
     );
 }
 
