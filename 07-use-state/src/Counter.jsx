7485836555a1b0ec1512447f390f6f6b1c793cc6
Suneel <suneelhere8022@gmail.com>
2025-01-12 22:20:53 +0530

    added increment decrement reset counters

--- 07-use-state/src/Counter.jsx
+++ 07-use-state/src/Counter.jsx
@@ -6,14 +6,25 @@ function Counter(){
     const [count, setCount] = useState(0)
 
     const increment = () => {
-        console.log(count);        
+        console.log(count);
+        
         setCount(count + 1)
     }
 
+    const decrement = () => {
+        setCount(count - 1)
+    }
+
+    const reset = () =>{
+        setCount(0)
+    }
 
     return (
         <>
-            <button onClick={increment}>Count is: {count}</button>
+            <p>Count is: {count}</p>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+            <button onClick={reset}>Reset</button>
         </>
     )
 
